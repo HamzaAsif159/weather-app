ab91a74fe82bd72588f0436c365af2b54b41d832
Hamza Asif <98582615+Hamzi-codes@users.noreply.github.com>
2022-12-05 18:09:14 +0500

    updated file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import LeftSide from "./ViewSides/leftSide"
+import RightSide from "./ViewSides/rightSide"
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <LeftSide />
+      <RightSide />
     </div>
   )
 }
--- src/ViewSides/leftSide.jsx
+++ src/ViewSides/leftSide.jsx
@@ -3,6 +3,7 @@ import moment from "moment"
 
 export default function LeftSide() {
   const [inputText, setInputText] = useState("")
+
   function handleChange(event) {
     setInputText(event.target.value)
   }
@@ -10,29 +11,54 @@ export default function LeftSide() {
   return (
     <div className="leftSide">
       <div className="leftTop">
-        <h1>{moment().format("MMM Do YY")}</h1>
+        <h2 className="dateTime">{moment().format("MMM Do, YY")}</h2>
         <form className="form">
-          <input type="text" value={inputText} onChange={handleChange} />
+          <input
+            type="text"
+            value={inputText}
+            onChange={handleChange}
+            placeholder="Search city"
+            className="searchBar"
+            required
+          />
+          <button className="searchBtn">Search</button>
         </form>
       </div>
       <div className="leftLower">
         <h3 className="overview">Today Overview</h3>
         <div className="weatherDetails">
           <div className="detailBox">
-            <h3>Wind</h3>
-            <h3>8.1 mph</h3>
+            <img src="/icons/wind.png" a lt="wind-icon" className="icon" />
+
+            <h3 className="figureHead">Wind</h3>
+            <h3 className="figure">8.1 mph</h3>
           </div>
           <div className="detailBox">
-            <h3>Highest/Lowest</h3>
-            <h3>55/45</h3>
+            <img
+              src="/icons/thermometer.svg"
+              alt="themometer-icon"
+              className="icon"
+            />
+            <h3 className="figureHead">Highest/Lowest</h3>
+            <h3 className="figure">55/45</h3>
           </div>
           <div className="detailBox">
-            <h3>Pressure</h3>
-            <h3>45</h3>
+            <img
+              src="/icons/pressure.svg"
+              alt="pressure-icon"
+              className="icon"
+            />
+            <h3 className="figureHead">Pressure</h3>
+            <h3 className="figure">45</h3>
           </div>
           <div className="detailBox">
-            <h3>Humidity</h3>
-            <h3>90%</h3>
+            <img
+              src="/icons/leaves-two.svg"
+              alt="humidity-icon"
+              className="icon"
+            />
+            <h3 className="figureHead">Humidity</h3>
+            <h3 className="figure">90%</h3>
           </div>
         </div>
       </div>
